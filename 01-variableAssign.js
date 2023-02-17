39745fbdbd0f28f48e14c96ed0b617e343b923a3
nikhilphadtare827@gmail.com <nikhilphadtare827@gnail.com>
2023-02-17 11:18:08 +0530

    variableAssign.js
    variableAssign.js

---
+++ 01-variableAssign.js
@@ -0,0 +1,31 @@
+console.log("********* Solution:1 ********");
+var city;
+console.log(city);
+
+var MyfullName="Nikhil phadtare"
+ 
+console.log("My fullname is:  "  + MyfullName );
+
+  console.log(">>>>>>>Update fullName<<<<<<<<<");
+  var MyfullName ="Nikhil KaKaSaheab phadtare";
+  console.log("MyfullName with middel Name:  "  + MyfullName);
+  console.log("********Solution:2*******");
+  var MyLove ="i love only js";
+  console.log(MyLove);
+  console.log(">>>>>>>Solution:3<<<<<<<<<<");
+  var firstName ="Nikhil"
+  console.log("My first name is: "  + firstName);
+  var lastName ="Phadtare";
+  console.log("My last name is: " + lastName);
+  var age="23";
+  console.log("My age is: " + age);
+  var address="Solapur"
+  console.log("My address is: " + address);
+  var pinCode="413253";
+  console.log("My city pin Code: " + pinCode);
+  var gender="Male"
+  console.log("gender: " + gender);
+   
+
+
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,15 @@
 console.log("Nikhil phadtare");
+console.log("31");
+console.log("pune");
+
+var fullName="Nikhil phadtare";
+console.log(fullName);
+console.log(fullName);
+
+var city; //variable Declaration
+console.log(city);
+city="pune"; //variable initailization
+console.log(city);
+city="surat"; //variable updeate
+console.log(city);
+
