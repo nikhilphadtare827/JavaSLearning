c0fd355635745c4348e396cdf1fdac68525529fb
nikhilphadtare827@gmail.com <nikhilphadtare827@gnail.com>
2023-02-17 14:18:44 +0530

    helloWoeld.js
    helloWoeld.js

---
+++ 02_ assigndatatypes.js
@@ -0,0 +1,20 @@
+console.log("---------Before Initialization Data Type-----------");
+
+var BankName;
+var DataType;
+console.log("Bank Name:", typeof BankName);
+console.log("Data Type: ", typeof DataType);
+
+console.log("--------After Initialization Data Type-------");
+
+var BankName = "SBI Bank";
+var typeofDataType = typeof DataType;
+console.log("Bank name : "  + BankName);
+console.log("Data Type: ", typeof DataType);
+console.log("--------------------------------------------");
+var mark = 90;
+var DataType;
+var typeofmark = typeof mark
+console.log("mark is:", mark);
+
+
---
+++ 03_datatypes.js
@@ -0,0 +1,35 @@
+var myName = "nikhil phadtare";
+var fatherName = "kakasaheb"
+
+var typeofmyName = typeof myName
+console.log("tytypeofmyNamepeof variable myName: ", );
+console.log("typeof variable fatherName: ", typeof fatherName);
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false;
+var typeofage = typeof age
+console.log("typeof age is : ", age);
+console.log("typeof weight is: ", weight);
+
+var typeofisMarried = typeof isMarried
+console.log("typeof variable isMarride: ", typeof isMarried);
+
+var typeofislazy = typeof isLazy
+console.log("typeof variable isLazy: ", typeof isLazy);
+
+var graduation;
+console.log("value of graduation variable is: ", graduation );
+console.log("typeof graduation is: ", graduation );
+
+var city = null
+console.log("value of city variable is:", city);
+console.log("typeof city is: ", typeof city);
+
+var alphabet = "A"
+
+
+
+
